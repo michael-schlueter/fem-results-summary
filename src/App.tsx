f5d388afaabd2e9db84f27a766b062b1a30005f6
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-05 16:58:16 +0200

    add html markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
 function App() {
-  return <p className="text-red-500">Hello world</p>;
+  return (
+    <div>
+      <div>
+        <section>
+          <h1>Your result</h1>
+          <div>
+            <p>76</p>
+            <p>of 100</p>
+          </div>
+          <h2>Great </h2>
+          <p>
+            Your performance exceeds 65% of the people conducting the test here!
+          </p>
+        </section>
+        <section>
+          <h3>Summary</h3>
+          <ul>
+            <li>
+              <span>Reaction</span>
+              <span>80 / 100</span>
+            </li>
+            <li>
+              <span>Memory</span>
+              <span>92 / 100</span>
+            </li>
+            <li>
+              <span>Verbal</span>
+              <span>61 / 100</span>
+            </li>
+            <li>
+              <span>Reaction</span>
+              <span>73 / 100</span>
+            </li>
+          </ul>
+          <button>Continue</button>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
