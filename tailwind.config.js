d56fc4170c6dd01f10253f6f37916e527393da4f
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-05 17:37:08 +0200

    add custom colors, font-family and font-size to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-red": "hsl(0, 100%, 67%)",
+        "organgey-yellow": "hsl(39, 100%, 56%)",
+        "green-teal": "hsl(166, 100%, 37%)",
+        "cobalt-blue": "hsl(234, 85%, 45%)",
+        "light-slate-blue": "hsl(252, 100%, 67%)",
+        "light-royal-blue": "hsl(241, 81%, 54%)",
+        "violet-blue": "hsla(256, 72%, 46%, 1)",
+        "persian-blue": "hsla(241, 72%, 46%, 0)",
+        "pale-blue": "hsl(221, 100%, 96%)",
+        "light-lavender": "hsl(241, 100%, 89%)",
+        "dark-gray-blue": "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        "hanken-grotesk": ['"Hanken Grotesk"', "sans-serif"],
+      },
+      fontSize: {
+        "3.5xl": ["32px", "auto"],
+      }
+    },
   },
   plugins: [],
-}
+};
