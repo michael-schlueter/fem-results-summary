435f7ce5ce4322748a31839c0d037d2c4964821f
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-06 23:53:28 +0200

    add layout for first section

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ function App() {
   return (
     <div className="w-full min-h-screen">
       <div>
-        <section className="flex flex-col text-center bg-gradient-to-t from-light-royal-blue to-light-slate-blue">
+        <section className="flex flex-col text-center bg-gradient-to-t rounded-b-[32px] from-light-royal-blue to-light-slate-blue">
           <h1 className="text-light-lavender text-lg mt-6">Your Result</h1>
           <div className="w-[140px] h-[140px] mx-auto flex flex-col justify-center items-center mt-6 rounded-full bg-gradient-to-t from-persian-blue to-violet-blue">
             <p className="text-white text-5.5xl font-extrabold">76</p>
             <p className="text-light-lavender/50">of 100</p>
           </div>
           <h2 className="text-white text-2xl font-bold mt-6">Great </h2>
-          <p className="text-light-lavender mt-2 mb-10">
+          <p className="max-w-[260px] text-light-lavender mt-2 mx-auto mb-10">
             Your performance exceeds 65% of the people conducting the test here!
           </p>
         </section>
