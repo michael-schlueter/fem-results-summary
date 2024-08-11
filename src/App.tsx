4eb878321cc0cb544c3e9da831d8ae27b9605d63
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-11 17:29:07 +0200

    convert to grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,28 @@ function App() {
   );
 
   return (
-    <div className="w-full min-h-screen md:flex md:justify-center md:items-center">
-      <main className="md:flex md:justify-center max-w-[736px] md:rounded-[32px] md:shadow-custom">
-        <section className="flex flex-col text-center bg-gradient-to-t rounded-b-[32px] md:rounded-[32px] md:pt-[38px] from-light-royal-blue to-light-slate-blue md:max-w-[50%] md:px-12">
-          <h1 className="text-light-lavender text-lg md:text-2xl mt-6 md:mt-0">
-            Your Result
-          </h1>
-          <div className="w-[140px] h-[140px] md:w-[200px] md:h-[200px] mx-auto flex flex-col justify-center items-center mt-6 md:mt-[35px] rounded-full bg-gradient-to-t from-persian-blue to-violet-blue">
-            <p className="text-white text-5.5xl md:text-7xl font-extrabold">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center">
+      <main className="grid grid-cols-[minmax(auto, 736px)] md:grid-cols-[minmax(auto,375px)_minmax(auto,375px)] md:grid-rows-[1fr] md:rounded-[32px] md:shadow-custom">
+        <section className="grid place-items-center text-center bg-gradient-to-t rounded-b-[32px] md:rounded-[32px] p-12 pt-6 md:pt-12 from-light-royal-blue to-light-slate-blue">
+          <h1 className="text-light-lavender text-lg md:text-2xl">Your Result</h1>
+          <div className="w-[140px] h-[140px] md:w-[200px] md:h-[200px] my-6 mx-auto flex flex-col justify-center items-center rounded-full bg-gradient-to-t from-persian-blue to-violet-blue">
+            <span className="text-white text-5.5xl md:text-7xl font-extrabold">
               {averageScore}
-            </p>
-            <p className="text-light-lavender/50 md:text-lg">of 100</p>
+            </span>
+            <span className="text-light-lavender/50 md:text-lg">of 100</span>
           </div>
-          <h2 className="text-white text-2xl md:text-3.5xl font-bold mt-6 md:mt-[28px]">
+          <h2 className="text-white text-2xl md:text-3.5xl font-bold mb-6">
             Great{" "}
           </h2>
           <p className="max-w-[260px] md:max-w-none text-light-lavender md:text-lg mt-2 md:mt-[14px] mx-auto mb-10">
             Your performance exceeds 65% of the people conducting the test here!
           </p>
         </section>
-        <section className="flex flex-col gap-6 md:gap-0 pt-6 md:pt-[38px] pb-[30px] px-[30px]">
+        <section className="p-6 md:p-12">
           <h3 className="text-lg md:text-2xl font-bold text-dark-gray-blue">
             Summary
           </h3>
-          <ul className="flex flex-col gap-4 md:mt-[28px]">
+          <ul className="grid gap-4 my-6 mx-0">
             {data.map((item, index) => (
               <li
                 key={index}
@@ -72,7 +70,7 @@ function App() {
               </li>
             ))}
           </ul>
-          <button className="rounded-[128px] bg-dark-gray-blue text-white font-bold py-4 px-[120px] md:text-lg outline-none md:focus-visible:bg-gradient-to-t md:focus-visible:from-light-royal-blue md:focus-visible:to-light-slate-blue md:mt-[41px]">
+          <button className="rounded-[128px] bg-dark-gray-blue text-white font-bold w-full py-3 px-0 md:text-lg outline-none border-none cursor-pointer md:focus-visible:bg-gradient-to-t md:focus-visible:from-light-royal-blue md:focus-visible:to-light-slate-blue">
             Continue
           </button>
         </section>
@@ -85,8 +83,5 @@ export default App;
 
 /*
 TODO:
-- convert to Grid
-- make the app totally responsive
-- fix the button (e.g. no border)
 - componentalize
 */
